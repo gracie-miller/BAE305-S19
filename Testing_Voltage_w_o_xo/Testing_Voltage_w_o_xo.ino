1763258293508c2decc68b8ce393d52cd6cc8432
gracie-miller <46763290+gracie-miller@users.noreply.github.com>
2019-03-03 19:20:06 -0500

    Add files via upload

---
+++ Testing_Voltage_w_o_xo/Testing_Voltage_w_o_xo.ino
@@ -0,0 +1,22 @@
+float threshold = 3.0;
+
+void setup() {
+  // put your setup code here, to run once:
+Serial. begin (9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+float sensorValueP = analogRead(A5)*(5.0/1024.0);
+float sensorValueD = analogRead (A4) *(5.0/1024.0);
+
+Serial.print (sensorValueP);
+Serial.print ("V\t");
+
+
+Serial.print (sensorValueD);
+Serial.println ("V");
+
+
+delay(10);
+}
---
+++ reverse/reverse.ino
@@ -0,0 +1,42 @@
+const int PMWA = 5;
+const int AIN1 = 2;
+const int AIN2 = 4;
+const int STBY = 3;
+const int PMWB = 6;
+const int BIN2 = 8;
+const int BIN1 = 7;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  digitalWrite (STBY, HIGH);
+  digitalWrite (AIN1, LOW);
+  digitalWrite (AIN2, HIGH);
+  digitalWrite (BIN1, LOW);
+  digitalWrite (BIN2, HIGH);
+
+analogWrite (PMWB, 179); // car goes straight for approximately 10 feet.
+analogWrite (PMWA, 175);
+delay (12000);
+
+analogWrite (PMWB, 0); // car stops for 1 second;
+analogWrite (PMWA, 0);
+delay (1000);
+
+ digitalWrite (STBY, HIGH);
+  digitalWrite (AIN1, HIGH);
+  digitalWrite (AIN2, LOW);
+  digitalWrite (BIN1, HIGH);
+  digitalWrite (BIN2, LOW);
+
+analogWrite (PMWB, 179); // car goes straight for approximately 10 feet in the reverse direction.
+analogWrite (PMWA, 175);
+delay (12000);
+
+}
