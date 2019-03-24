4d61c0c7d5ab5891ef64f2b3dae5783b6acb3e08
gracie-miller <46763290+gracie-miller@users.noreply.github.com>
2019-03-24 13:39:19 -0400

    Add files via upload

---
+++ Lab 08/fan.ino
@@ -0,0 +1,24 @@
+
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin (9600);
+pinMode(7, OUTPUT);
+}
+
+void loop() {
+ delay (1000); // put your main code here, to run repeatedly:
+for (int i=0; i<=100; i++)
+{
+  Serial.println (i);
+ analogWrite (7, i+130);
+  delay(250);
+}
+delay (1000);
+for (int i= 100; i>=0; i--)
+{Serial.println(i);
+analogWrite (7, i+120);
+delay (250);
+}
+analogWrite(7,0);
+}
